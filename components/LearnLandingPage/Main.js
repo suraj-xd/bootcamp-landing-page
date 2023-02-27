9c85cd741186369db866a286af6bdfa5d4be514d
Suraj <unofficialartist3@gmail.com>
2023-02-27 12:28:48 +0530

    irogre

--- components/LearnLandingPage/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function LandingPage(){
-    return(
-        <div className="main">
-            
-        </div>
-    )
-}
--- components/UserProfile/UserProfile copy 2/Loading.js
+++
@@ -1,9 +0,0 @@
-function Loading() {
-  return (
-    <div>
-      <h1>Loading</h1>
-    </div>
-  );
-}
-
-export default Loading;
